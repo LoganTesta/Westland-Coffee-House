8d67523803927107187530f604bf26698db0186a
Logan Testa <logan.testa@outlook.com>
2019-05-06 12:52:57 -0700

    Add content to locations, rework nav labels

--- PortlandCoffeeShop/assets/include/navigation-content.php
+++ PortlandCoffeeShop/assets/include/navigation-content.php
@@ -1,7 +1,7 @@
 <nav id="desktopNav">
     <ul>
         <li><a href="about.php" class="navLink">About</a></li>
-        <li><a href="locations-and-hours.php" class="navLink">Location and Hours</a></li>
+        <li><a href="locations-and-hours.php" class="navLink">Location</a></li>
         <li><a href="menu.php" class="navLink">Menu</a></li>
         <li><a href="events.php" class="navLink">Events</a></li>
         <li><a href="contact-us.php" class="navLink">Contact Us</a></li>
@@ -10,7 +10,7 @@
 <nav id="mobileNav">
     <ul>
         <li><a href="about.php">About</a></li>
-        <li><a href="locations-and-hours.php">Location and Hours</a></li>
+        <li><a href="locations-and-hours.php">Location</a></li>
         <li><a href="menu.php">Menu</a></li>
         <li><a href="events.php">Events</a></li>
         <li><a href="contact-us.php">Contact Us</a></li>
--- PortlandCoffeeShop/locations-and-hours.php
+++ PortlandCoffeeShop/locations-and-hours.php
@@ -20,7 +20,7 @@ require_once 'connect.php';
             <header>
                 <?php include 'assets/include/logo.php'; ?>
                 <?php include 'assets/include/header-content.php'; ?>
-                <h2 id="subTitle">Locations and Hours</h2>
+                <h2 id="subTitle">Location</h2>
             </header>
 
             <?php include 'assets/include/navigation-content.php'; ?>
@@ -28,12 +28,51 @@ require_once 'connect.php';
             <div id="content">
                 <div class="content-row">
                     <div class="col-sma-6">
-                        <h3>Location and Hours</h3>
-                        <p>Locations.  Hours.</p>
+                        <h3>Coffee Shop Hours</h3>
+                        <table>
+                            <thead>
+                            <td>Day</td>
+                            <td>Hours</td>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Monday</td>
+                                    <td>6AM-Midnight</td>
+                                </tr>
+                                <tr>
+                                    <td>Tuesday</td>
+                                    <td>6AM-Midnight</td>
+                                </tr>
+                                <tr>
+                                    <td>Wednesday</td>
+                                    <td>6AM-Midnight</td>
+                                </tr>
+                                <tr>
+                                    <td>Thursday</td>
+                                    <td>6AM-Midnight</td>
+                                </tr>
+                                <tr>
+                                    <td>Friday</td>
+                                    <td>6AM-1PM</td>
+                                </tr>
+                                <tr>
+                                    <td>Saturday</td>
+                                    <td>6AM-1PM</td>
+                                </tr>
+                                <tr>
+                                    <td>Sunday</td>
+                                    <td>8AM-Midnight</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div>
+                            <h3>Location</h3>
+                            <p>33322 SW 11th Avenue Portland, Oregon 97205</p>
+                        </div>
                     </div>
 
                     <div class="col-sma-6">
-                       
+
                     </div>
                 </div>
             </div>
