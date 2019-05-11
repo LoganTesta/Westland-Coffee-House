c639cfb10555b5813747974dbf6492ffbd10a83e
Logan Testa <logan.testa@outlook.com>
2019-05-10 21:27:27 -0700

    Switched footer social images to font-awesome icons, styling

--- WestsideCoffeeHouse/assets/include/footer-content.php
+++ WestsideCoffeeHouse/assets/include/footer-content.php
@@ -1,10 +1,16 @@
 <footer>
-
     <div class="footer-copyright">
         &copy; <?php echo date("Y"); ?> Westside Coffee House. All Rights Reserved.</p>
         <div class="social-container">
             <div class="social-logo facebook">
-                <a href="#" target="_blank"><img class="social-icon" src="assets/images/social/facebook-logo.png" alt="Facebook Logo" /></a>
+                <a href=""><i class="fab fa-facebook-f fa-2x social-icon"></i>
+                </a>
+            </div>
+            <div class="social-logo instagram">
+                <a href=""><i class="fab fa-instagram fa-2x social-icon"></i></a>
+            </div>
+            <div class="social-logo twitter">
+                <a href=""><i class="fab fa-twitter fa-2x social-icon"></i></a>
             </div>
         </div>
 </footer>
