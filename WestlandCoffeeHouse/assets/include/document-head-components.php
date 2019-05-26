0cde04abe156dc6653f21d74e6d7e9591d564c64
Logan Testa <logan.testa@outlook.com>
2019-05-26 13:46:20 -0700

    Subheader styling adjustments including mobile

--- WestlandCoffeeHouse/assets/include/document-head-components.php
+++ WestlandCoffeeHouse/assets/include/document-head-components.php
@@ -4,7 +4,7 @@
 <link rel="icon" type="image/png" href="assets/images/favicon.png" />
 <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="assets/css/main-styles.css?mod=05262019" />
+<link rel="stylesheet" type="text/css" href="assets/css/main-styles.css?mod=05262019V2" />
 <link rel="stylesheet" type="text/css" href="assets/css/print-styles.css?mod=04292019" media="print" />
 
 
