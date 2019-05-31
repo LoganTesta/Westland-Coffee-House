496feaddfa0b94dfc25a762efaff161ceaec0bfd
Logan Testa <logan.testa@outlook.com>
2019-05-30 21:24:18 -0700

    Rework and improve contact form error flagging

--- WestlandCoffeeHouse/assets/javascript/contact-form-validation.js
+++ WestlandCoffeeHouse/assets/javascript/contact-form-validation.js
@@ -3,51 +3,51 @@ var userName;
 var userEmail;
 var userComments;
 
+var submitButtonClicked = false;
+
 function validateContactForm() {
-    alert("validation time");
+
     var userName = document.forms["contactOurCoffeeShop"]["userName"];
     var userEmail = document.forms["contactOurCoffeeShop"]["userEmail"];
     var userComments = document.forms["contactOurCoffeeShop"]["userComments"];
-    
-    
-    var atPosition = userEmail.indexOf("@");
-    var dotPosition = userEmail.lastIndexOf(".");
-    var lastEmailCharacter = userEmail.length - 1;
-    
-    if(!(userName.value === null)) {
-        userName.classList.replace(" required-field-needed", "");
+
+
+    if (userName.value !== "") {
+        userName.classList.remove("required-field-needed");
     }
-    if(!(userComments.value === null)) {
-        userName.classList.replace(" required-field-needed", "");
+
+    if (userComments.value !== "") {
+        userComments.classList.remove("required-field-needed");
     }
-    
-    
-    var validEmail=true;
-    
-        if(!(userEmail.value === null)) {
-            validEmail=false;
+
+
+    var validEmail = true;
+
+    var atPosition = userEmail.value.indexOf("@");
+    var dotPosition = userEmail.value.lastIndexOf(".");
+    var lastEmailCharacter = userEmail.value.length - 1;
+
+    if (userEmail.value === "") {
+        validEmail = false;
     }
-        /*If the @ position is at the start (or less) position of value 0,  validContactForm=false. */
+    /*If the @ position is at the start (or less) position of value 0,  validContactForm=false. */
     if (atPosition <= 0) {
-        validContactForm = false;
+        validEmail = false;
     }
 
     /* There must be at least 1 character after the @ position and the last dot position. */
     else if (atPosition + 1 >= dotPosition) {
-        validContactForm = false;
+        validEmail = false;
     }
 
     /* There must be at least two characters after the last "." symbol.  */
     else if (dotPosition + 1 >= lastEmailCharacter) {
-        validContactForm = false;
-    } else if (userComments === null || userComments === "") {
-        validContactForm = false;
-    }
-    
-    if(validEmail===false){
-         userName.classList.replace(" required-field-needed", "");
+        validEmail = false;
     }
 
+    if (validEmail) {
+        userEmail.classList.remove("required-field-needed");
+    }
 }
 
 
@@ -55,14 +55,23 @@ var contactButton = document.getElementById("contactButton");
 contactButton.addEventListener("click", toggleRequiredFields, "false");
 
 function toggleRequiredFields() {
-    validateContactForm();
+
     var userName = document.forms["contactOurCoffeeShop"]["userName"];
     var userEmail = document.forms["contactOurCoffeeShop"]["userEmail"];
     var userComments = document.forms["contactOurCoffeeShop"]["userComments"];
-    
+
     userName.className += " required-field-needed";
     userEmail.className += " required-field-needed";
     userComments.className += " required-field-needed";
+
+
+    submitButtonClicked = true;
+    if (submitButtonClicked) {
+        setInterval(function () {
+            validateContactForm();
+        }, 10);
+    }
+
 }
 
 
