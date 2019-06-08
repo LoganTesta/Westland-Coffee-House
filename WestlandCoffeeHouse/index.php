fd6fcfc420e44a0f7da64320e9c09d58685e16f7
Logan Testa <logan.testa@outlook.com>
2019-06-07 20:18:01 -0700

    Add index content, styling

--- WestlandCoffeeHouse/index.php
+++ WestlandCoffeeHouse/index.php
@@ -34,8 +34,11 @@
                                 <div class="col-sma-7">
                                     <h3>The Place to Grab Coffee and Meet up</h3>
                                     <div class="subheader-divider"></div>
-                                    <p>Welcome to our Westland Coffee House.  We have the finest coffee and are reputed to be one of the best places in Portland 
-                                        to grab coffee and hangout.</p>
+                                    <p>Welcome to our Westland Coffee House!</p>
+                                    <p>We have the finest coffee and are reputed to be one of the best places 
+                                        in Portland to grab coffee and gather.</p>
+                                    <p>Plenty of seating inside and outdoors!</p>
+                                    <p>Located in downtown SW Portland at 33322 SW 10th Avenue Portland, Oregon 97205.</p>
                                 </div>
                                 <div class="col-sma-5">
                                     <div class="item-image-container">
@@ -50,8 +53,17 @@
                                 <div class="col-sma-7 float-right-above-a-breakpoint">
                                     <h3>Top Notch Coffee and Hearty Food</h3>
                                     <div class="subheader-divider"></div>
-                                    <p>We have all your favorite hot, chilled beverages.  We also serve a wide variety of tasty food, ranging from bagels, 
-                                        soups, salads, sandwiches, and more!</p>
+                                    <p>We have all your favorite hot and chilled beverages, coffee, smoothies, and more:</p>                                                                 
+                                    <ul class="bulleted-list food-beverage-list">
+                                        <li>hot coffee</li>
+                                        <li>cold coffee and smoothies</li>
+                                        <li>oatmeal/waffles/etc.</li>
+                                        <li>bagels</li>
+                                        <li>sandwiches</li>
+                                        <li>soups</li>
+                                        <li>featured blends</li>
+                                        <li>...and more!</li>
+                                    </ul>                                    
                                 </div>
                                 <div class="col-sma-5 float-right-above-a-breakpoint">
                                     <div class="item-image-container">
@@ -67,6 +79,10 @@
                                     <h3>Our Shop is Open</h3>
                                     <div class="subheader-divider"></div>
                                     <p>But don't take our word for it, come on by and see for yourself!</p>
+                                    <p>We frequently get new blends too!</p>
+                                    <p>Don't see something on our menu?</p>
+                                    <p><a href="contact-us.php">Contact us</a> or let us know in store and we'll 
+                                        see what we can do!</p>
                                 </div>
                                 <div class="col-sma-5">
                                     <div class="item-image-container">
