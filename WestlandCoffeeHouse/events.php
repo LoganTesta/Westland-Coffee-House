bbd6b5e99796d3275d56d836f0ae6b4b11db31cc
Logan Testa <logan.testa@outlook.com>
2019-06-08 12:15:02 -0700

    Adjust mobile table padding

--- WestlandCoffeeHouse/events.php
+++ WestlandCoffeeHouse/events.php
@@ -17,7 +17,7 @@
                     <?php include 'assets/include/logo.php'; ?>
                     <?php include 'assets/include/header-content.php'; ?>
                     <div class="subtitle-container">
-                    <h2 id="subTitle">Events</h2>
+                        <h2 id="subTitle">Events</h2>
                     </div>
                 </div>
             </header>
@@ -29,50 +29,51 @@
                     <div class="col-sma-12">
                         <h3>Events</h3>
                         <p>We frequently host fun community events.  Check back often to see what's coming up.</p>
-                        <table class="events-table">
-                            <thead>
-                                <tr>
-                                    <th class="event-name">Event</th>
-                                    <th class="date-time">Date/Time</th>
-                                    <th class="event-info">More Info</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Local Jazz Musician</td>
-                                    <td>Thursday March 10, 7-9 PM</td>
-                                    <td>Free local concert!</td>
-                                </tr>
-                                <tr>
-                                    <td>Settlers of Catan Tournament</td>
-                                    <td><strong>Every 1st Saturday of the month 7-10 PM</strong></td>
-                                    <td>Come have some friendly competition in Settlers of Catan.  In-house Gift Card to prize of $50 to winner!</td>
-                                </tr>
-                                <tr>
-                                    <td>Quarterly Art Show/Gallery</td>
-                                    <td>Saturday, April 18, 11-5PM</td>
-                                    <td>Want to get your artwork out for the public to see?  Want to decorate a cool coffee shop and maybe get a few
-                                        sales? <a href="contact-us.php">Contact Us and let's talk!</a></td>
-                                </tr>
-                                <tr>
-                                    <td>Food Drive</td>
-                                    <td>March 1st-31st</td>
-                                    <td>Bring in fresh canned and boxed food to help locals in need through the Oregon Food Bank.  We proudly support 
-                                        local charities in our community!</td>
-                                </tr>
-                                <tr>
-                                    <td>Piano Hour with Local Musician</td>
-                                    <td>Thursday May 15, 8-9 PM</td>
-                                    <td>Free local concert!</td>
-                                </tr>
-                                <tr>
-                                    <td>Local Indie Band</td>
-                                    <td>Thursday April 25, 7-9 PM</td>
-                                    <td>Free local concert!</td>
-                                </tr>
-                            </tbody>
-
-                        </table>
+                        <div class="table-container">
+                            <table class="events-table">
+                                <thead>
+                                    <tr>
+                                        <th class="event-name">Event</th>
+                                        <th class="date-time">Date/Time</th>
+                                        <th class="event-info">More Info</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Local Jazz Musician</td>
+                                        <td>Thursday March 10, 7-9 PM</td>
+                                        <td>Free local concert!</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Settlers of Catan Tournament</td>
+                                        <td><strong>Every 1st Saturday of the month 7-10 PM</strong></td>
+                                        <td>Come have some friendly competition in Settlers of Catan.  In-house Gift Card to prize of $50 to winner!</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Quarterly Art Show/Gallery</td>
+                                        <td>Saturday, April 18, 11-5PM</td>
+                                        <td>Want to get your artwork out for the public to see?  Want to decorate a cool coffee shop and maybe get a few
+                                            sales? <a href="contact-us.php">Contact Us and let's talk!</a></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Food Drive</td>
+                                        <td>March 1st-31st</td>
+                                        <td>Bring in fresh canned and boxed food to help locals in need through the Oregon Food Bank.  We proudly support 
+                                            local charities in our community!</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Piano Hour with Local Musician</td>
+                                        <td>Thursday May 15, 8-9 PM</td>
+                                        <td>Free local concert!</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Local Indie Band</td>
+                                        <td>Thursday April 25, 7-9 PM</td>
+                                        <td>Free local concert!</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
