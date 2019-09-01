db12809f2aed3027331d15a102523a4e40eac8d6
Logan Testa <logan.testa@outlook.com>
2019-09-01 16:21:48 -0700

    Refactor- check for !=, ==, and condense validation code

--- WestlandCoffeeHouse/validate-contact-form.php
+++ WestlandCoffeeHouse/validate-contact-form.php
@@ -7,25 +7,20 @@ $UserEmail;
 $UserComments;
 $SendEmailTo = "logan.testa@outlook.com";
 $Subject = "Questions/Comments About Westland Coffee House";
-$UserName = trim($_POST['userName']);
-$UserName = strip_tags($UserName);
-$UserName = htmlspecialchars($UserName);
-$UserEmail = trim($_POST['userEmail']);
-$UserEmail = strip_tags($UserEmail);
-$UserEmail = htmlspecialchars($UserEmail);
-$UserComments = trim($_POST['userComments']);
-$UserComments = strip_tags($UserComments);
-$UserComments = htmlspecialchars($UserComments);
+$UserName = htmlspecialchars(strip_tags(trim($_POST['userName'])));
+$UserEmail = htmlspecialchars(strip_tags(trim($_POST['userEmail'])));
+$UserComments = htmlspecialchars(strip_tags(trim($_POST['userComments'])));
+
 
 /* Validation time */
 $PassedValidation = true;
-if (Trim($UserName) == "") {
+if (Trim($UserName) === "") {
     $PassedValidation = false;
 }
-if (Trim($UserEmail) == "") {
+if (Trim($UserEmail) === "") {
     $PassedValidation = false;
 }
-if (Trim($UserComments) == "") {
+if (Trim($UserComments) === "") {
     $PassedValidation = false;
 }
 
@@ -33,7 +28,7 @@ if (Trim($UserComments) == "") {
 if (!filter_var($UserEmail, FILTER_VALIDATE_EMAIL)) {
     $PassedValidation = false;
 }
-if ($PassedValidation == false) {
+if ($PassedValidation === false) {
     print "Sorry validation failed.  Please check all fields again.";
     exit;
 }
@@ -54,7 +49,7 @@ if ($SuccessfulSubmission) {
     <?php
 
     print "<meta http-equiv=\"refresh\" content=\"0;URL=contact-us.php\">";
-} else if ($SuccessfulSubmission == false) {
+} else if ($SuccessfulSubmission === false) {
     ?>
     <script type="text/javascript">
         alert("Submission failed. Please try again.");
