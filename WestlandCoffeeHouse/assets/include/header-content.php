c1accddc37873e696881ef9492470ca256f951e1
Logan Testa <logan.testa@outlook.com>
2019-10-15 14:57:57 -0700

    Update header-content

--- WestlandCoffeeHouse/assets/include/header-content.php
+++ WestlandCoffeeHouse/assets/include/header-content.php
@@ -1,14 +1,3 @@
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-114894181-1"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'UA-114894181-1');
-        </script>
-
-
 <div class="title-container">
     <h1 id="mainTitle"><a href="index.php">Westland Coffee House</a></h1>
 </div>
-        
